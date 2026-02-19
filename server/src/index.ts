c1d4eb51258032f793a33a3bf7bd0be603ec5cbd
Jay <hawkinsjr27@gmail.com>
2026-02-19 10:03:51 -0500

    feat: implemented cowsay feature-flag example in typescript

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,58 @@
 import express from "express";
+import cowsay from "cowsay";
+import { OpenFeature, InMemoryProvider, type EvaluationContext } from "@openfeature/server-sdk";
 
 /// Declare and set up express application
 
-const app = express()
-const port = 3000
+const app = express();
+const routes = express.Router();
+const port = 3000;
+
+// Set up middleware
+app.use((_, res, next) => {
+  res.setHeader("content-type", "text/plain");
+  next();
+}, routes);
+
+const featureFlags = OpenFeature.getClient();
+
+// Set up an alias so we can easily identify request contexts
+type RequestContext = any;
+
+const FLAG_CONFIGURATION = {
+  'with-cows': {
+    variants: {
+      on: true,
+      off: false
+    },
+    disabled: false,
+    defaultVariant: "on",
+    contextEvaluator: (context: EvaluationContext) => {
+      if (context.cow === "Bessie") {
+        return "on";
+      }
+      return "off";
+    }
+  }
+};
+
+const featureFlagProvider = new InMemoryProvider(FLAG_CONFIGURATION);
+
+OpenFeature.setProvider(featureFlagProvider);
 
 // Configure root endpoint for GET
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get('/', async (req, res) => {
+  const context: EvaluationContext = {
+    cow: req.get("x-cow") ?? ""
+  };
+  const withCows = await featureFlags.getBooleanValue('with-cows', false, context);
+  if (withCows) {
+    res.send(cowsay.say({ text: "Hello World!" }));
+  } else {
+    res.send('Hello World!');
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Apsis server listening on port ${port}`)
-})
+  console.log(`Apsis server listening on port ${port}`);
+});
