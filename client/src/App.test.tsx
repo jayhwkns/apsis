130e0bc42b94fcdc3b344c1b5eb62f67a9ebc9ea
Jay <hawkinsjr27@gmail.com>
2026-02-19 12:51:18 -0500

    chore: cleaned items that won't be used from client

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('basic test', () => {
+  fail("Not implemented")
+  // render(<App />);
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
 });
