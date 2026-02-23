2b2e1ba1c0956e033d12147a7e325584e3a0efa1
Jay <hawkinsjr27@gmail.com>
2026-02-23 12:17:17 -0500

    feat: footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,21 @@ function App() {
         <DateDisplay date={date} setDate={setDate} />
         <ApodContainer date={date} />
       </main>
+      <footer class="bg-white text-black p-4 mt-40">
+        <h3 class="font-bold">Apsis</h3>
+        <small>
+          A modernized view of <a href="https://apod.nasa.gov/apod/astropix.html"> NASA's Astronomy Photo of the Day Website</a>
+          <br />
+          Developed by Jay Hawkins | <a href="https://github.com/jayhwkns">GitHub</a>
+          <br />
+          <a href="https://github.com/jayhwkns/apsis">Apsis GitHub</a>
+          <br />
+          The website and it's creators are in no way affiliated with NASA
+          <br />
+          There may be some inaccuracies in the content of this site. Notice anything usual?
+          Open an issue <a href="https://github.com/jayhwkns/apsis/issues">here</a>.
+        </small>
+      </footer>
     </>
   )
 }
